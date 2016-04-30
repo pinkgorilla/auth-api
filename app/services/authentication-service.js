fc7f7f2dcacf9d650c9a60eef25a4e1ac0a82b61
Tris Setiawan <tris.setiawan@yahoo.com>
2016-04-30 14:04:31 +0700

    change authentication token result + docs
    docs on authenticate endpoint

--- app/services/authentication-service.js
+++ app/services/authentication-service.js
@@ -2,34 +2,45 @@
 
 var Service = require('./service');
 var Account = require('capital-models').identity.Account;
+var map = require('capital-models').map;
 var jwt = require('jsonwebtoken');
 var config = require('../../config');
 
 module.exports = class AuthenticationService extends Service {
     constructor() {
         super("1.0.0");
-        this.collectionName = "accounts";
     }
 
     authenticate(request, response, next) {
         var username = request.body.username;
         var query = { username: username };
 
-        request.single(this.collectionName, query)
+        request.single(map.identity.account, query)
             .then(account => {
-                if (!account)
+                if (!account) {
                     reponse.locals.data = { success: false, message: "Authentication failed. Account not found" };
+                    next();
+                }
                 else if (account) {
-                    if (account.password != request.body.password)
+                    if (account.password != request.body.password) {
                         reponse.locals.data = { success: false, message: "Authentication failed. Invalid username or password" };
+                        next();
+                    }
                     else {
-                        var tokenOption = {};//{ expiresInMinutes: 1440 };
-                        var token = jwt.sign(account, config.secret, tokenOption);
-                        response.locals.data = { success: true, token: token }
+                        var loadProfile = request.single(map.identity.userProfile, { accountId: account._id });
+                        var loadInfo = request.single(map.identity.userOrganizationInfo, { accountId: account._id });
+                        Promise.all([loadProfile, loadInfo])
+                            .then(results => {
 
+                                var tokenOption = {};//{ expiresInMinutes: 1440 };
+                                var data = Object.assign({}, results[1], results[0], account);
+                                var token = jwt.sign(data, config.secret, tokenOption);
+                                response.locals.data = { success: true, token: token }
+                                next();
+                            })
+                            .catch(e => next(e));
                     }
                 }
-                next();
             })
             .catch(e => next(e));
     }
---
+++ docs/endpoints/authenticate.md
@@ -0,0 +1,13 @@
+end point to authenticate user.
+# URI
+    /authenticate
+
+## HTTP METHODS
+available http methods
+* ### POST
+    * ##### header
+    * ##### params
+        * username          
+        * password
+    * ##### return
+        * {success:bool, token:string}
