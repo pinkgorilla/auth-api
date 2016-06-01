85592e6dc4c2e47329e776a375c01251dad01239
Tris Setiawan <tris.setiawan@yahoo.com>
2016-06-01 17:27:13 +0700

    1.0.3
    implement authentication-mod

--- server.js
+++ server.js
@@ -1,37 +1,38 @@
-var express = require('express');
-var app = express();
-
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-var toolkit = require('mean-toolkit');
-app.use(toolkit.middlewares.enableCors);
-
-var passport = require('passport');
-app.use(passport.initialize());
-
+var factory = require('mongo-factory');
 var config = require('./config');
-var jwt = require('mean-toolkit').passport.jwt;
-jwt.strategy(function (payload, done) {
-    return done(null, payload.user);
-}, config.secret);
 
-var morgan = require('morgan');
-app.use(morgan('dev'));  
+factory.getConnection(config.connectionString)
+    .then(db => {
 
-var authenticationRouter = require('./app/routers/authentication-router');
-app.use('/authenticate', authenticationRouter);
+        var express = require('express');
+        var app = express();
 
-var accountsRouter = require('./app/routers/accounts-router');
-app.use('/accounts', accountsRouter);
+        var bodyParser = require('body-parser');
+        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(bodyParser.json());
 
-var meRouter = require('./app/routers/me-router');
-app.use('/me', meRouter);
+        var toolkit = require('mean-toolkit');
+        app.use(toolkit.middlewares.enableCors);
 
-app.use(toolkit.middlewares.resultFormatter);
-app.use(toolkit.middlewares.errorFormatter);
+        var passport = require('passport');
+        app.use(passport.initialize());
 
-var port = process.env.PORT || 8080;
-app.listen(port);
-console.log('Magic happens at http://localhost:' + port); 
+        var morgan = require('morgan');
+        app.use(morgan('dev'));
+
+        var controllerOptions = { jwt: { secret: config.secret } };
+        var AuthenticationController = require('authentication-mod').controllers.AuthenticationController;
+        app.use('/authenticate', new AuthenticationController(db, controllerOptions).router);
+
+        var AccountController = require('authentication-mod').controllers.AccountController;
+        app.use('/accounts', new AccountController(db, controllerOptions).router);
+
+        var MeController = require('authentication-mod').controllers.MeController;
+        app.use('/me', new MeController(db, controllerOptions).router);
+
+
+
+        var port = process.env.PORT || 8080;
+        app.listen(port);
+        console.log('Magic happens at http://localhost:' + port);
+    });
